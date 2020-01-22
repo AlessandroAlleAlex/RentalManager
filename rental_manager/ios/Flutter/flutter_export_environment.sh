498cd1d5b7fe15702b778ef86324867829e82ff9
Alessandro <34758570+AlessandroAlleAlex@users.noreply.github.com>
2020-01-22 14:35:14 -0800

    starter code

---
+++ rental_manager/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/alessandro/development/Flutter_tools/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/alessandro/Documents/ECS193/RentalManager/rental_manager"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/alessandro/development/Flutter_tools/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
