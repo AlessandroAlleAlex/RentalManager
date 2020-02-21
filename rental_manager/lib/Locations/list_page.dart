72229626b92a3ed3d1e5fee683f5eb9ad54161fe
AlessandroAlleAlex <alle95liu@gmail.com>
2020-02-20 22:17:47 -0800

    resized location selection tiles

--- rental_manager/lib/Locations/list_page.dart
+++ rental_manager/lib/Locations/list_page.dart
@@ -63,12 +63,19 @@ class _ListPageState extends State<ListPage> {
       child: InkWell(
         onTap: () => navigateToCategory(snapshot.data[index]),
         child: Container(
+          padding: const EdgeInsets.fromLTRB(0, 0, 0, 18),
+          height: 200,
           decoration: BoxDecoration(
             boxShadow: [
               BoxShadow(
-                color: Colors.grey,
-                blurRadius: 20.0, // has the effect of softening the shadow
-              ),
+                color: Colors.blue,
+                blurRadius: 100.0, // has the effect of softening the shadow
+                spreadRadius: 0, // has the effect of extending the shadow
+                offset: Offset(
+                  30.0, // horizontal, move right 10
+                  0.0, // vertical, move down 10
+                ),
+              )
             ],
           ),
           child: Card(
@@ -76,7 +83,7 @@ class _ListPageState extends State<ListPage> {
               decoration: BoxDecoration(
                 image: DecorationImage(
                   image: NetworkImage(snapshot.data[index].data['imageURL']),
-                  fit: BoxFit.cover,
+                  fit: BoxFit.fill,
                 ),
               ),
               child: Padding(
