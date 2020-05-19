17f1c540647735e801076f49fc6840027274bb2d
GreatJingTJ <43863396+GreatJingTJ@users.noreply.github.com>
2020-05-18 21:58:08 -0700

    Delete rate.dart

--- rental_manager/lib/rate.dart
+++
@@ -1,2 +0,0 @@
-// cit from https://github.com/sarbagyastha/flutter_rating_bar/blob/master/lib/flutter_rating_bar.dart
-// use package import 'package:flutter_rating_bar/flutter_rating_bar.dart'; cite in the yaml file 
